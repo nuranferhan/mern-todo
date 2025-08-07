a0c250c3c40ec68f7c0c23c35cc8a1336480aec0
ferhan <nuranferhan@gmail.com>
2025-08-08 01:30:03 +0300

    editing

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,22 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 require("./conn/conn");
 const auth = require("./routes/auth");
 const list = require("./routes/list");
-
 app.use(express.json());
 app.use(cors());
-
 app.get("/", (req, res) => {
-    res.send("MERN Todo API is running!");
+    res.send("Hello");
 }); 
 
 app.use("/api/v1", auth);
 app.use("/api/v2", list);
 
-// Vercel için port ayarı
-const port = process.env.PORT || 1000;
-app.listen(port, () => { 
-  console.log(`Server Started on port ${port}`);
+app.listen(1000, () => { 
+  console.log("Server Started");
 });
 
-// Vercel için export
-module.exports = app;
+
+
